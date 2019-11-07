5df4c7562ac4768abce2369069f65513f50fa0f9
Karl Hornlund <karlhornlund@gmail.com>
2019-11-07 19:22:59 +1030

    Moved where Unet uses dropout

--- segmentation_models_pytorch/unet/decoder.py
+++ segmentation_models_pytorch/unet/decoder.py
@@ -7,8 +7,13 @@ from ..base.model import Model
 
 
 class DecoderBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, use_batchnorm=True, attention_type=None,
-                 dropout=0, weight_std=False
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        use_batchnorm=True,
+        attention_type=None,
+        weight_std=False
     ):
         super().__init__()
         if attention_type is None:
@@ -17,8 +22,6 @@ class DecoderBlock(nn.Module):
         elif attention_type == 'scse':
             self.attention1 = SCSEModule(in_channels)
             self.attention2 = SCSEModule(out_channels)
-
-        self.dropout = nn.Dropout(p=dropout, inplace=False)
 
         self.block = nn.Sequential(
             Conv2dReLU(in_channels, out_channels, kernel_size=3, padding=1,
@@ -38,7 +41,6 @@ class DecoderBlock(nn.Module):
         else:
             x = F.interpolate(x, scale_factor=2, mode='nearest')
 
-        x = self.dropout(x)
         x = self.block(x)
         x = self.attention2(x)
         return x
@@ -76,17 +78,17 @@ class UnetDecoder(Model):
         out_channels = decoder_channels
 
         self.layer1 = DecoderBlock(in_channels[0], out_channels[0],
-                                   use_batchnorm, attention_type, dropout, weight_std)
+                                   use_batchnorm, attention_type, weight_std)
         self.layer2 = DecoderBlock(in_channels[1], out_channels[1],
-                                   use_batchnorm, attention_type, dropout, weight_std)
+                                   use_batchnorm, attention_type, weight_std)
         self.layer3 = DecoderBlock(in_channels[2], out_channels[2],
-                                   use_batchnorm, attention_type, dropout, weight_std)
+                                   use_batchnorm, attention_type, weight_std)
         self.layer4 = DecoderBlock(in_channels[3], out_channels[3],
-                                   use_batchnorm, attention_type, dropout, weight_std)
+                                   use_batchnorm, attention_type, weight_std)
         self.layer5 = DecoderBlock(in_channels[4], out_channels[4],
-                                   use_batchnorm, attention_type, dropout, weight_std)
+                                   use_batchnorm, attention_type, weight_std)
         self.final_conv = nn.Conv2d(out_channels[4], final_channels, kernel_size=(1, 1))
-
+        self.dropout = nn.Dropout(p=dropout)
         self.initialize()
 
     def compute_channels(self, encoder_channels, decoder_channels):
@@ -111,6 +113,7 @@ class UnetDecoder(Model):
         x = self.layer3([x, skips[2]])
         x = self.layer4([x, skips[3]])
         x = self.layer5([x, None])
+        x = self.dropout(x)
         x = self.final_conv(x)
 
         return x
